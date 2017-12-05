5b2487976f2ec73ff3d1b7b650f460e3f992e7f6
Eva Roth <eva-maria.roth@helsinki.fi>
2017-12-05 23:58:13 +0200

    finishing chapter 5

--- Data/create_human.R
+++ Data/create_human.R
@@ -21,22 +21,22 @@ summary(gii)
 # Renaming the variables
 colnames(hd)
 colnames(hd)[1]<-"HDI.rank"
-colnames(hd)[2]<-"country"
+colnames(hd)[2]<-"Country"
 colnames(hd)[3]<-"HDI"
-colnames(hd)[4]<-"lifeexp"
-colnames(hd)[5]<-"exp.edu"
+colnames(hd)[4]<-"Life.Exp"
+colnames(hd)[5]<-"Edu.Exp"
 colnames(hd)[6]<-"mean.edu"
-colnames(hd)[7]<-"GNI.cap"
+colnames(hd)[7]<-"GNI"
 colnames(hd)[8]<-"GNI.rank-HDI.rank"
 colnames(hd)
 
 colnames(gii)
 colnames(gii)[1]<-"GII.rank"
-colnames(gii)[2]<-"country"
+colnames(gii)[2]<-"Country"
 colnames(gii)[3]<-"GII"
-colnames(gii)[4]<-"matermor"
-colnames(gii)[5]<-"adobr"
-colnames(gii)[6]<-"reppar"
+colnames(gii)[4]<-"Mat.Mor"
+colnames(gii)[5]<-"Ado.Birth"
+colnames(gii)[6]<-"Parli.F"
 colnames(gii)[7]<-"edu2F"
 colnames(gii)[8]<-"edu2M"
 colnames(gii)[9]<-"labF"
@@ -46,17 +46,63 @@ colnames(gii)
 #Mutating the "Gender inequality" data in order to create two new variables.
 #First accessing the tidyverse package dplyr
 library(dplyr)
-gii <- mutate(gii, ratio.edu2 = gii$edu2F/gii$edu2M)
-gii <- mutate(gii, ratio.lab = gii$labF/gii$labM)
+gii <- mutate(gii, Edu2.FM = gii$edu2F/gii$edu2M)
+gii <- mutate(gii, Labo.FM = gii$labF/gii$labM)
 
 colnames(gii)  
 summary(gii$ratio.edu2)  
 summary(gii$ratio.lab)  
 
 #Joining the two datasets using the variable country as the identifier.  
-human <- inner_join(hd, gii, by = "country")
+human <- inner_join(hd, gii, by = "Country")
 dim(human)
 #The dataset human has 195 observations of 19 variables
 
 setwd("/Users/eva-mariaroth/Documents/GitHub/IODS-project")
 write.table(human, file = "human.csv", sep = ",", col.names = TRUE)
+
+#Part two Data wringling - R Studio exercise numer 5
+#Mutating data. Transforming Gross National Income (GNI) variable to numeric, using string manipulation
+dim(human)
+str(human$GNI)
+library(stringr)
+GNI2 <- str_replace(human$GNI, pattern=",", replace ="") %>% as.numeric
+human <- mutate(human, GNI2)
+str(human$GNI2)
+dim(human)
+colnames(human)
+
+#Selecting certain variables and excluding the rest.
+keep <- c("Country", "Edu2.FM", "Labo.FM", "Life.Exp", "Edu.Exp", "GNI2", "Mat.Mor", "Ado.Birth", "Parli.F")
+human <- select(human, one_of(keep))
+#Renaming the column GNI2 (the variable GNI, transformed into numeric) back to GNI
+colnames(human)[6]<-"GNI"
+colnames(human)
+
+#Removing all the rows with missing values
+complete.cases(human)
+data.frame(human[-1], comp = complete.cases(human))
+human_ <- filter(human, complete.cases(human))
+str(human_)
+# 162 observations of 9 variables
+
+#The last seven observations are not concerning countries, but regions. We want to exclude them from the dataset.
+tail(human_, n=10)
+last <- nrow(human) -7
+human_ <- human_ [1:155,]
+str(human_)
+
+#Defining the row names by the country names and removing the country name column from the data.
+rownames(human_) <- human_$Country
+str(human_)
+human_ <- select(human_, -Country)
+str(human_)
+#155 observations of 8 variables.
+
+#Saving the data
+write.table(human_, file = "human_.csv", sep = ",", row.names = TRUE, col.names = TRUE)
+#Reopening it to see if everything worked out fine.
+test <- read.csv("/Users/eva-mariaroth/Documents/GitHub/IODS-project/human_.csv", header = TRUE, sep= ",")
+glimpse(test)
+#155 observations of 8 variables
+
